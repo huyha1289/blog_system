c252afab426e5bf8eacb675585b5b9981c2c6edd
Ha Van Huy <ha.van.huy@framgia.com>
2013-12-24 16:10:06 +0700

    date time

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131216094015) do
+ActiveRecord::Schema.define(version: 20131236094015) do
 
   create_table "comments", force: true do |t|
     t.string   "content"
